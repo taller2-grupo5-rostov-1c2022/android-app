47ed2ebf1405871d953daa4d84d554d5be40ec0b
Tomas Civini <tcivini@gmail.com>
2022-04-14 14:54:26 -0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,52 +1,15 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { DefaultTheme, Text, Headline, Button, TextInput, Provider as PaperProvider } from 'react-native-paper';
-import { StyleSheet, Image, SafeAreaView} from "react-native";
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import LoginScreen from './components/LoginScreen'
 
 export default function App() {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
 
   return (
     <PaperProvider theme={theme}>
-      <SafeAreaView style={styles.container}>
-        <Image source={require('./img/logo.png')} />
-        <Headline>Spotifiuby</Headline>
-        <Text>Log In</Text>
-        <TextInput
-          placeholder="Email"
-          value={email}
-          onChangeText={text => setEmail(text)}
-          style={styles.input}
-        />
-        <TextInput
-          secureTextEntry={true}
-          placeholder="Password"
-          value={password}
-          onChangeText={text => setPassword(text)}
-          style={styles.input}
-        />
-        <Button mode="contained">Log In</Button>
-        <StatusBar style="auto" />
-      </SafeAreaView>
+      <LoginScreen/>
     </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    width: "80%"
-  }
-});
 
 const theme = {
   ...DefaultTheme,
---
+++ src/components/LoginScreen.js
@@ -0,0 +1,48 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, Headline, Button, TextInput } from 'react-native-paper';
+import { StyleSheet, Image, SafeAreaView} from "react-native";
+
+const LoginScreen = () => {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    return (
+      <SafeAreaView style={styles.container}>
+        <Image source={require('../img/logo.png')} />
+        <Headline>Spotifiuby</Headline>
+        <Text>Log In</Text>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={styles.input}
+        />
+        <TextInput
+          secureTextEntry={true}
+          placeholder="Password"
+          value={password}
+          onChangeText={text => setPassword(text)}
+          style={styles.input}
+        />
+        <Button mode="contained">Log In</Button>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    );
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: "80%"
+  }
+});
+
+export default LoginScreen;
