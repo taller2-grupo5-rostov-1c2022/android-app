3a09f4b67e4c368be340688caaca446eddc0f857
nicolas_zulaica <nzulaica@fi.uba.ar>
2022-04-14 17:00:36 -0300

    fetch songs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from "react";
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
-import LoginScreen from './components/LoginScreen'
+import React, { useEffect, useState } from "react";
+
+import {
+  Text,
+  DefaultTheme,
+  Provider as PaperProvider,
+} from "react-native-paper";
+import LoginScreen from "./components/LoginScreen";
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await fetch(
+        "https://rostov-gateway.herokuapp.com/songs/"
+      );
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <PaperProvider theme={theme}>
-      <LoginScreen/>
+      <LoginScreen />
+      <Text>Songs: {isLoading ? "Loading" : JSON.stringify(data)}</Text>
     </PaperProvider>
   );
 }
@@ -16,7 +42,7 @@ const theme = {
   roundness: 2,
   colors: {
     ...DefaultTheme.colors,
-    primary: '#3498db',
-    accent: '#f1c40f',
+    primary: "#3498db",
+    accent: "#f1c40f",
   },
 };
