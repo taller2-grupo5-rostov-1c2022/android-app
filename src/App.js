ec752803b3b26ff3f255818e6375957b0bc4d3a3
fdelu <fdeluca@fi.uba.ar>
2022-04-16 01:20:28 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import {
   Text,
   DefaultTheme,
@@ -15,7 +15,6 @@ const TextScreen = ({ navigation }) => {
 };
 
 export default function App() {
-
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,38 +1,39 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Text, Headline, Button, TextInput } from 'react-native-paper';
-import { StyleSheet, Image, SafeAreaView} from "react-native";
+import { Text, Headline, Button, TextInput } from "react-native-paper";
+import { StyleSheet, Image, SafeAreaView } from "react-native";
 
 const LoginScreen = ({ navigation }) => {
-    const [email, setEmail] = React.useState("");
-    const [password, setPassword] = React.useState("");
-    return (
-      <SafeAreaView style={styles.container}>
-        <Image source={require('../img/logo.png')} />
-        <Headline>Spotifiuby</Headline>
-        <Text>Log In</Text>
-        <TextInput
-          placeholder="Email"
-          value={email}
-          onChangeText={text => setEmail(text)}
-          style={styles.input}
-        />
-        <TextInput
-          secureTextEntry={true}
-          placeholder="Password"
-          value={password}
-          onChangeText={text => setPassword(text)}
-          style={styles.input}
-        />
-        <Button
-        onPress={() =>
-          navigation.navigate('TextScreen')
-        }
-         mode="contained">Log In</Button>
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    );
-  }
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image source={require("../img/logo.png")} />
+      <Headline>Spotifiuby</Headline>
+      <Text>Log In</Text>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        style={styles.input}
+      />
+      <TextInput
+        secureTextEntry={true}
+        placeholder="Password"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        style={styles.input}
+      />
+      <Button
+        onPress={() => navigation.navigate("TextScreen")}
+        mode="contained"
+      >
+        Log In
+      </Button>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -45,8 +46,8 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
-    width: "80%"
-  }
+    width: "80%",
+  },
 });
 
 export default LoginScreen;
