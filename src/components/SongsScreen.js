6aee1bbc7a9eef0ffd3b3948c730be3576703a8a
Tomas Civini <tcivini@gmail.com>
2022-04-27 17:08:08 -0300

    added functional player

--- src/components/SongsScreen.js
+++ src/components/SongsScreen.js
@@ -1,32 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import { webApi, useSWR, json_fetcher } from "../util/services";
 import { Headline, List, ActivityIndicator } from "react-native-paper";
 import styles from "./styles.js";
 import ExternalView from "./ExternalView";
 import Player from "./Player";
-
+import appContext from "./appContext";
 
 export default function SongsScreen() {
   const songs = useSWR(webApi + "/songs/", json_fetcher);
-
+  const context = React.useContext(appContext);
   return (
     <ExternalView style={styles.container}>
       <Headline> Songs </Headline>
-      {content(songs.isValidating, songs.data, songs.error)}
+      {content(
+        songs.isValidating, songs.data, songs.error, context.setName, context.setArtist
+      )}
       <Player/>
     </ExternalView>
   );
 }
 
-function content(isLoading, data, error) {
+function content(isLoading, data, error, setName, setArtist) {
   if (isLoading) return <ActivityIndicator style={styles.activityIndicator} />;
 
   if (error) return <Headline>Error: {error.message}</Headline>;
 
-  return mapData(data);
+  return mapData(data, setName, setArtist);
 }
 
-function mapData(data) {
+function mapData(data, setName, setArtist) {
   return Object.entries(data).map(([key, value]) => {
     console.log(key);
     console.log(value);
@@ -35,6 +37,10 @@ function mapData(data) {
         title={value.name}
         description={"by " + value.artist_name}
         key={key}
+        onPress={()=>{
+          setName(value.name);
+          setArtist(value.artist_name);
+        }}
       />
     );
   });
