ad03ced5031b5b6b6c7b9dee6529dcfaa021d2c6
fdelu <fdeluca@fi.uba.ar>
2022-04-30 22:38:22 -0300

    Agrego string de error de firebase

--- src/components/login/firebaseErrors.js
+++ src/components/login/firebaseErrors.js
@@ -3,6 +3,7 @@ const firebaseErrors = {
   "auth/email-already-in-use": "The email is already in use",
   "auth/wrong-password": "Invalid email or password",
   "auth/user-disabled": "The user account has been disabled",
+  "auth/popup-closed-by-user": "",
 }; // list of firebase error codes to alternate error messages
 
 export default function errorMsg(errorCode) {
