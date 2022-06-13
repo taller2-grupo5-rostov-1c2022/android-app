3da46e8f1aee46d5d2132ed65387b035e1ce142b
fdelu <fdeluca@fi.uba.ar>
2022-06-13 00:31:38 -0300

    fix crudlist urls

--- src/components/account/managePlaylists/ManageMyPlaylists.js
+++ src/components/account/managePlaylists/ManageMyPlaylists.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import CrudList from "../../general/CrudList.js";
 import PlaylistDialog from "./PlaylistDialog";
 import PlaylistItem from "../../playlists/PlaylistItem.js";
-import { MY_PLAYLISTS_URL } from "../../../util/services.js";
+import { MY_PLAYLISTS_URL, PLAYLISTS_URL } from "../../../util/services.js";
 
 export default function ManageMyPlaylists() {
   return (
@@ -12,7 +12,7 @@ export default function ManageMyPlaylists() {
       editDialog={PlaylistDialog}
       itemComponent={PlaylistItem}
       emptyMessage="You don't have any playlists yet"
-      revalidateUrl={MY_PLAYLISTS_URL}
+      revalidateUrl={PLAYLISTS_URL}
     />
   );
 }
